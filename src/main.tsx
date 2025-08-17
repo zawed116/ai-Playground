81101bc2acb038430ca2d067173b9e11a6dbf340
Zawed <zawedakhter5786@gmail.com>
2025-08-17 23:11:56 +0530

    add

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found. Did you forget to add <div id='root'></div> in index.html?");
+}
+
+ReactDOM.createRoot(rootElement as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
