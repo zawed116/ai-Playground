7245b1caba830f47b5bd1fb3b4c6c7f03593e070
Zawed <zawedakhter5786@gmail.com>
2025-08-17 23:16:22 +0530

    add

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,10 @@ import "./index.css";
 
 const rootElement = document.getElementById("root");
 
-if (!rootElement) {
-  throw new Error("Root element not found. Did you forget to add <div id='root'></div> in index.html?");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-ReactDOM.createRoot(rootElement as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
